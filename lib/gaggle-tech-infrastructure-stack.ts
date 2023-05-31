f124a49f9412108db9c5c2b4cb25553315a05387
Shane Satterfield <1795838+shanesatterfield@users.noreply.github.com>
2023-05-31 00:07:17 -0700

    Add APIGateway definition to stack

--- lib/gaggle-tech-infrastructure-stack.ts
+++ lib/gaggle-tech-infrastructure-stack.ts
@@ -1,16 +1,37 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class GaggleTechInfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const restApi = new apigateway.RestApi(this, "GaggleTechApi", {
+      restApiName: "GaggleTechApi",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+      },
+      deploy: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GaggleTechInfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    restApi.root.addResource("health").addMethod(
+      "GET",
+      new apigateway.MockIntegration({
+        integrationResponses: [{ statusCode: "200" }],
+        passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+        requestTemplates: {
+          "application/json": JSON.stringify({ statusCode: 200 }),
+        },
+      })
+    );
+
+    new cdk.CfnOutput(this, "GaggleTechRestApiId", {
+      value: restApi.restApiId,
+      exportName: "GaggleTechRestApiId",
+    });
+    new cdk.CfnOutput(this, "GaggleTechRestApiRootResourceId", {
+      value: restApi.restApiRootResourceId,
+      exportName: "GaggleTechRestApiRootResourceId",
+    });
   }
 }
